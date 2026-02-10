5fdad22c76a4fd967331b035982049a0e416decc
JoeGuidoboni <joe.guidoboni@gmail.com>
2026-02-10 18:05:30 -0500

    reformatting

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const d = @import("engine/defs.zig");
 const position = @import("engine/position.zig");
 
 pub fn main() !void {
-    print("{s}\n\n\n", .{d.banner});
+    // print("{s}\n\n\n", .{d.banner});
 
     const starting_pos = try utils.fenToPosition("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     try starting_pos.print();
